ae85d20240f302cad0a465ba2ea5a8c93db41843
AndreiFenesan <fenesana@yahoo.com>
2023-03-06 22:21:38 +0200

    added specifications for StudentService

--- src/main/java/com/example/demo/services/StudentService.java
+++ src/main/java/com/example/demo/services/StudentService.java
@@ -3,7 +3,6 @@ package com.example.demo.services;
 import com.example.demo.domain.Student;
 import com.example.demo.exception.ServiceException;
 import com.example.demo.repositories.StudentRepo;
-import com.example.demo.validators.StudentValidator;
 import com.example.demo.validators.ValidationError;
 import com.example.demo.validators.Validator;
 import com.google.common.hash.Hashing;
@@ -21,8 +20,18 @@ public class StudentService {
     private final StudentRepo studentRepo;
     private final Validator<Student> studentValidator;
 
+    /**
+     * @param firstName String, firstName of the student we want to add.
+     * @param lastName String, lastName of the student we want to add.
+     * @param username Unique string representing the username of the student.
+     * @param password String, the student password.
+     * @return the added student.
+     * @throws ValidationError if the student is not valid.
+     * @throws ServiceException if there is already a student in the database with the same username.
+     */
+
     public Student addStudent(String firstName, String lastName, String username, String password) throws ValidationError, ServiceException {
-        Student student = new Student(firstName,lastName,username,password,LocalDateTime.now());
+        Student student = new Student(firstName, lastName, username, password, LocalDateTime.now());
         studentValidator.validate(student);
         if (this.studentRepo.findStudentByUsername(student.getUsername()).isPresent()) {
             throw new ServiceException("Student with this username already exists");
@@ -33,10 +42,18 @@ public class StudentService {
         return studentRepo.save(student);
     }
 
-    public Optional<Student> findStudentByUsername(String username){
+    /**
+     * @param username Unique string, representing the username of the student.
+     * @return Optional<Student> representing te student which has the int parameter username
+     */
+    public Optional<Student> findStudentByUsername(String username) {
         return studentRepo.findStudentByUsername(username);
     }
 
+    /**
+     *
+     * @return all students found
+     */
     public List<Student> getAllStudents() {
         return studentRepo.findAll();
     }
