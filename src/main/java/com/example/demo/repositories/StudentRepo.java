910fd2051583dc6fa0a538434cdb15b0c84a7123
AndreiFenesan <fenesana@yahoo.com>
2023-03-10 18:59:43 +0200

    Added find student by group functionality.

--- src/main/java/com/example/demo/repositories/StudentRepo.java
+++ src/main/java/com/example/demo/repositories/StudentRepo.java
@@ -3,8 +3,10 @@ package com.example.demo.repositories;
 import com.example.demo.domain.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepo extends MongoRepository<Student, String> {
      Optional<Student> findStudentByUsername(String username);
+     List<Student> findStudentsByGroup(Integer group);
 }
--- src/main/java/com/example/demo/services/StudentService.java
+++ src/main/java/com/example/demo/services/StudentService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -51,6 +52,10 @@ public class StudentService {
      */
     public List<Student> getAllStudents() {
         return studentRepo.findAll();
+    }
+
+    public List<Student> getStudentsFromGroup(Integer group) {
+        return this.studentRepo.findStudentsByGroup(group);
     }
 }
 
